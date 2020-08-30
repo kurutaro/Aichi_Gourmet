d48864db8027016cdce17228e774d1e7200e095c
橋立直弥 <naoya@MacBook-Pro.local>
2020-08-30 17:39:48 +0900

    mySQLに変更

--- aichi/settings.py
+++ aichi/settings.py
@@ -76,10 +76,19 @@ WSGI_APPLICATION = 'aichi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # 変更
+        'NAME': 'database_name', # プロジェクトで使用するデータベース名
+        'USER': 'root', # パソコンにインストールしたMySQLのユーザー名
+        'PASSWORD': '', # 同上。そのパスワード
     }
 }
 
