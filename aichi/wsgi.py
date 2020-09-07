2db1c4d8e91706530303d0cdf13447896f8087de
橋立直弥 <naoya@MacBook-Pro.local>
2020-09-07 15:08:56 +0900

    herokuの設定2

--- aichi/wsgi.py
+++ aichi/wsgi.py
@@ -17,3 +17,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aichi.settings')
 
 application = get_wsgi_application()
+
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
